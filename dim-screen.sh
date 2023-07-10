8c4bf477f51a9ef19e965bae5944a7ae55074f7e
eylles <ed.ylles1997@gmail.com>
2023-07-10 13:46:52 -0600

    protoype scripts

---
+++ dim-screen.sh
@@ -0,0 +1,165 @@
+#!/bin/sh
+
+# Example notifier script -- lowers screen brightness, then waits to be killed
+# and restores previous brightness on exit.
+
+# return type: boolean
+# usage: is_num "value"
+# description: check if passed value is a number
+is_num() {
+    printf %f "$1" >/dev/null 2>&1
+}
+
+## CONFIGURATION ##############################################################
+
+# type: int
+# def: min_brightness=0
+# description:
+#    The lowest brightness value.
+#    Brightness will be lowered to this value.
+min_brightness=0
+
+# type: int
+sleep_pid=""
+
+# If your video driver works with xbacklight, set -time and -steps for fading
+# to $min_brightness here. Setting steps to 1 disables fading.
+# fade_time=200
+# fade_steps=10
+
+# If you have a driver without RandR backlight property (e.g. radeon), set this
+# to use the sysfs interface and create a .conf file in /etc/tmpfiles.d/
+# containing the following line to make the sysfs file writable for group
+# "users":
+#
+#     m /sys/class/backlight/acpi_video0/brightness 0664 root users - -
+#
+# sysfs_path="/sys/class/backlight/*/brightness"
+
+# Time to sleep (in seconds) between increments. If unset or
+# empty, fading is disabled.
+fade_step_time=0
+# the steps to change brightness per cycle. By default is 1.
+dim_step=1
+
+###############################################################################
+
+# return type: int
+# usage: get_brightness
+# description:
+#    will return the current brightness value
+#    brightness values are from 0 to 255
+get_brightness() {
+    if [ -z "$sysfs_path" ]; then
+        brightnessctl g
+    else
+        cat "$sysfs_path"
+    fi
+}
+
+# return type: void
+# usage: set_brightness num
+# description:
+#    will set the brightness to the passed number
+#    brighness values are from 0 to 255
+set_brightness() {
+    [ -n "$dbgOUT" ] && printf 'brightness level: %s\n' "$1"
+    if [ -z "$sysfs_path" ]; then
+        brightnessctl s "$1" >/dev/null
+    else
+        echo "$1" > "$sysfs_path"
+    fi
+}
+
+# return type: void
+# usage: reset_brightness
+# description:
+#    will run set_brightness
+#    on the variable "$current_brightness"
+reset_brightness() {
+    set_brightness "$current_brightness"
+}
+
+# return type: void
+# usage: fade_brightness "$num"
+# description:
+#    num will be the value
+#    to which the birhgtness
+#    will be faded to.
+fade_brightness() {
+    if [ -z "$fade_step_time" ]; then
+        set_brightness "$1"
+    else
+        # type: int
+        # def: level=$(get_brightness)
+        # description:
+        #    the return value of get_brightness
+        level=$(get_brightness)
+        while [ "$level" -gt "$min_brightness" ]; do
+            # type: int
+            # def: level=$((level-dim_step))
+            # description:
+            #    value of level minus dim step
+            level=$((level-dim_step))
+            if [ "$level" -lt 0 ]; then
+                level=0
+            fi
+            set_brightness "$level"
+            sleep "$fade_step_time"
+        done
+    fi
+}
+
+sig_handler() {
+    if kill -0 "$sleep_pid" 2>/dev/null; then
+        kill "$sleep_pid"
+        { [ -n "$dbgOUT" ] || [ -n "$VERB" ]; } && printf "%s %s: sleep %s killed.\n" "$(date +"%F %T")" "${0##*/}" "$sleep_pid"
+    fi
+    exit 0
+}
+
+################
+# --- main --- #
+################
+
+while [ $# -gt 0 ]; do
+    case $1 in
+        -step-time)
+            if is_num "$2"; then
+                fade_step_time=$2
+            fi
+            ;;
+        -dim-step)
+            if is_num "$2"; then
+                dim_step=$2
+            fi
+            ;;
+        -debug)
+            dbgOUT=1
+            ;;
+        -verbose)
+            VERB=1
+            ;;
+    esac
+    shift
+done
+# ${fade_step_time:-0.1}
+case $fade_step_time in
+    0) fade_step_time=0.1 ;;
+    0.0*) fade_step_time=0.1 ;;
+esac
+
+case $dim_step in
+    0) dim_step=1 ;;
+esac
+
+{ [ -n "$dbgOUT" ] || [ -n "$VERB" ]; } && printf "%s %s: PID: %s dimming.\n" "$(date +"%F %T")" "${0##*/}" "$$"
+
+trap 'sig_handler' TERM INT HUP
+trap 'reset_brightness' EXIT
+current_brightness=$(get_brightness)
+fade_brightness $min_brightness
+sleep 2147483647 &
+sleep_pid=$!
+{ [ -n "$dbgOUT" ] || [ -n "$VERB" ]; } && printf "%s %s: waiting for %s sleep.\n" "$(date +"%F %T")" "${0##*/}" "$sleep_pid"
+wait "$sleep_pid"
---
+++ saver.sh
@@ -0,0 +1,222 @@
+#!/bin/sh
+
+#####################
+# --- variables --- #
+#####################
+
+# type:        string
+# description:
+#   screen saver module to use
+Screen_Saver=""
+
+saver_list_1="walldir,livewall"
+saver_list_2="currwall"
+saver_list_3="matrix,pipes,snake"
+saver_list_4="walldir,livewall"
+saver_list_5="livewall"
+
+# type:        int bool
+# description:
+#   C like int bool, whether to print debug output.
+#   0 = false
+#   1 = true
+DBGOUT=""
+# type:        int bool
+# description:
+#   C like int bool, whether to dry run.
+#   0 = false
+#   1 = true
+DRYRUN=""
+
+# type:        string
+# description:
+#   x11 screen geometry string WIDTHxHEIGHT
+geometry=$(xdpyinfo | grep 'dimensions:' | cut -d' ' -f7)
+
+# type:        int
+# description:
+#   current screen saver module pid
+#   used by sig_handler to kill the running screen saver module.
+saver_pid=""
+
+#####################
+# --- functions --- #
+#####################
+
+# return type: string
+# usage: split_str "string" "pattern"
+# description:
+#   splits string on pattern returns
+#   a newline separated list.
+# taken from: https://github.com/dylanaraps/pure-sh-bible#split-a-string-on-a-delimiter
+split_str() {
+    # Disable globbing.
+    # This ensures that the word-splitting is safe.
+    set -f
+
+    # Store the current value of 'IFS' so we
+    # can restore it later.
+    old_ifs=$IFS
+
+    # Change the field separator to what we're
+    # splitting on.
+    IFS=$2
+
+    # Create an argument list splitting at each
+    # occurance of '$2'.
+    #
+    # This is safe to disable as it just warns against
+    # word-splitting which is the behavior we expect.
+    # shellcheck disable=2086
+    set -- $1
+
+    # Print each list value on its own line.
+    printf '%s\n' "$@"
+
+    # Restore the value of 'IFS'.
+    IFS=$old_ifs
+
+    # Re-enable globbing.
+    set +f
+}
+
+# return type: void
+# usage: run_saver
+# description:
+#   will run the screen saver module specified by the
+#   Screen_Saver variable, and set the saver_pid variable
+#   after that it will wait until the screen saver module
+#   is terminated.
+run_saver() {
+  if [ "$DRYRUN" = 1 ]; then
+    printf '%s\n' "${0##*/}: dry run mode, no saver started."
+  else
+    # printf '%s\n' "${0##*/}: saver module $Screen_Saver selected."
+    case "$Screen_Saver" in
+      matrix)
+        [ "$DBGOUT" = 1 ] && printf '%s\n' "${0##*/}: starting saver $Screen_Saver"
+        xterm -fa "BlexMono Nerd Font Mono" -fs 12 -into "$XSCREENSAVER_WINDOW" -g "$geometry" -e unimatrix -af -s 95 &
+        saver_pid=$!
+        [ "$DBGOUT" = 1 ] && printf '%s\n' "${0##*/}: saver pid $saver_pid"
+        ;;
+      pipes)
+        np=$(shuf -n 1 -e 2 3 4 5 6 7 8 9)
+        [ "$DBGOUT" = 1 ] && printf '%s\n' "${0##*/}: starting saver $Screen_Saver"
+        xterm -fa "BlexMono Nerd Font Mono" -fs 30 -into "$XSCREENSAVER_WINDOW" -g "$geometry" -e pipes.sh -p "$np" -f 60 -R -r 8000 &
+        saver_pid=$!
+        [ "$DBGOUT" = 1 ] && printf '%s\n' "${0##*/}: saver pid $saver_pid"
+        ;;
+      snake)
+        sl=$(shuf -n 1 -e fancy dots)
+        [ "$DBGOUT" = 1 ] && printf '%s\n' "${0##*/}: starting saver $Screen_Saver"
+        xterm -fa "BlexMono Nerd Font Mono" -fs 30 -into "$XSCREENSAVER_WINDOW" -g "$geometry" -e sssnake -m screensaver -s 15 -l "$sl" &
+        saver_pid=$!
+        [ "$DBGOUT" = 1 ] && printf '%s\n' "${0##*/}: saver pid $saver_pid"
+        ;;
+      fire)
+        [ "$DBGOUT" = 1 ] && printf '%s\n' "${0##*/}: starting saver $Screen_Saver"
+        xterm -into "$XSCREENSAVER_WINDOW" -g "$geometry" -e fire -l 300 -t -s 10 -f 3 &
+        saver_pid=$!
+        [ "$DBGOUT" = 1 ] && printf '%s\n' "${0##*/}: saver pid $saver_pid"
+        ;;
+      walldir)
+        [ "$DBGOUT" = 1 ] && printf '%s\n' "${0##*/}: starting saver $Screen_Saver"
+        delay=$(shuf -n 1 -e 0.5 1 1.5 2 2.5 3)
+        find ~/Pictures/wallpapers -type f | shuf | nsxiv -i -bfq -S "$delay"\
+        -e "$XSCREENSAVER_WINDOW" -g "$geometry" -s F 2>/dev/null &
+        saver_pid=$!
+        [ "$DBGOUT" = 1 ] && printf '%s\n' "${0##*/}: saver pid $saver_pid"
+        # find ~/Pictures/wallpapers -type f | shuf |\
+        # mpv --no-input-terminal --loop=inf --no-stop-screensaver\
+        # --wid="${XSCREENSAVER_WINDOW}" --no-config --hwdec=auto\
+        # --vo=gpu --image-display-duration="$delay"
+        ;;
+      currwall)
+        [ "$DBGOUT" = 1 ] && printf '%s\n' "${0##*/}: starting saver $Screen_Saver"
+        nsxiv -bfq -e "$XSCREENSAVER_WINDOW" -g "$geometry"\
+        -s F ~/.local/share/bg 2>/dev/null &
+        saver_pid=$!
+        [ "$DBGOUT" = 1 ] && printf '%s\n' "${0##*/}: saver pid $saver_pid"
+        # mpv --no-input-terminal --loop=inf --no-stop-screensaver\
+        # --wid="${XSCREENSAVER_WINDOW}" --no-config --hwdec=auto\
+        # --vo=gpu --video-unscaled=yes ~/.local/share/bg
+        ;;
+      livewall)
+        [ "$DBGOUT" = 1 ] && printf '%s\n' "${0##*/}: starting saver $Screen_Saver"
+        mpv --no-input-terminal --loop=inf --no-stop-screensaver\
+        --wid="${XSCREENSAVER_WINDOW}" --no-config --hwdec=auto\
+        --really-quiet --no-audio\
+        --vo=gpu "$(shuf -n 1 -e ~/Videos/live-walls/* )" 2>/dev/null &
+        saver_pid=$!
+        [ "$DBGOUT" = 1 ] && printf '%s\n' "${0##*/}: saver pid $saver_pid"
+        ;;
+    esac
+    if kill -0 "$saver_pid"; then
+      wait
+    else
+      printf '%s\n' "${0##*/}: saver module failed, re-running."
+      run_saver
+    fi
+  fi
+}
+
+roll_saver() {
+  [ "$DBGOUT" = 1 ] && printf '%s\n' "SAVER_OPT: $SAVER_OPT"
+  if [ -z "$SAVER_OPT" ]; then
+    if [ -z "$Screen_Saver" ]; then
+      Screen_Saver=$(shuf -n 1 -e walldir currwall livewall)
+    fi
+  else
+    case "${SAVER_OPT}" in
+      0)  Screen_Saver=$(split_str "$saver_list_1" "," | shuf -n 1) ;;
+      2)  Screen_Saver=$(split_str "$saver_list_2" "," | shuf -n 1) ;;
+      10) Screen_Saver=$(split_str "$saver_list_3" "," | shuf -n 1) ;;
+      8)  Screen_Saver=$(split_str "$saver_list_4" "," | shuf -n 1) ;;
+      15) Screen_Saver=$(split_str "$saver_list_5" "," | shuf -n 1) ;;
+    esac
+  fi
+  [ "$DBGOUT" = 1 ] && printf '%s\n' "Screen_Saver: $Screen_Saver"
+}
+
+# return type: void
+# usage: trap 'sig_handler' SIGNAL
+# description:
+#   kills the current screen saver module with it's pid
+#   and then calls the run_saver function.
+#   it is called on the USR1 signal
+sig_handler() {
+  kill $saver_pid
+  roll_saver
+  run_saver
+}
+
+################
+# --- main --- #
+################
+
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        -debug)   DBGOUT=1  ;;
+        -dryrun)  DRYRUN=1  ;;
+        # xsecurelock passes to every saver on /usr/libexec/xsecurelock
+        # it is really only used on saver_xscreensaver tho.
+        -root)    :         ;;
+        *)
+          printf '%s\n' "${0##*/}: error, invalid argument: ${1} ignored."
+          # exit 1
+          ;;
+    esac
+    shift
+done
+
+[ "$DBGOUT" = 1 ] && printf '%s\n' "saver list 1: $saver_list_1"
+[ "$DBGOUT" = 1 ] && printf '%s\n' "saver list 2: $saver_list_2"
+[ "$DBGOUT" = 1 ] && printf '%s\n' "saver list 3: $saver_list_3"
+[ "$DBGOUT" = 1 ] && printf '%s\n' "saver list 4: $saver_list_4"
+[ "$DBGOUT" = 1 ] && printf '%s\n' "saver list 5: $saver_list_5"
+
+trap 'sig_handler' USR1
+
+roll_saver
+
+run_saver
