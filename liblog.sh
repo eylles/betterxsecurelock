520d8de2235c27c7ec8253c743f77be82c236db3
eylles <ed.ylles1997@gmail.com>
2026-05-25 15:04:49 -0600

    Add liblog.sh

---
+++ liblog.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# type: int
+# description: digit width of the process id number
+# default: 6
+PIDWIDTH="6"
+# use per OS pid length
+os_type=$(uname -s)
+pw=""
+case "${os_type}" in
+Linux)
+  pw=$(wc -c /proc/sys/kernel/pid_max | awk '{ print $1 }')
+  ;;
+NetBSD)
+  pidmax=30000
+  pw=$(printf '%s\n' "$pidmax" | wc -c)
+  ;;
+OpenBSD|FreeBSD|*BSD)
+  pidmax=99999
+  pw=$(printf '%s\n' "$pidmax" | wc -c)
+  ;;
+esac
+if [ -n "$pw" ]; then
+    PIDWIDTH="$pw"
+fi
+PIDWIDTH="$(( PIDWIDTH + 2 ))"
+
+# Usage: write_log "message text"
+# Return: void
+# Description:
+#   Write message to log file
+write_log () {
+    name="$myname"
+    message="$*"
+    printf '[%s] %12s %*s: %s\n' \
+        "$(date +'%Y-%m-%d %H:%M:%S')" \
+        "$name" \
+        "$PIDWIDTH" "[$mypid]" \
+        "$message" \
+        >> "$LOGFILE"
+}
+
