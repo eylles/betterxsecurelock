d49bba8e3f20cf462ce350305899ef6f0ffde99b
eylles <ed.ylles1997@gmail.com>
2026-05-25 13:51:43 -0600

    Add split_str to libutils.sh

--- libutils.sh
+++ libutils.sh
@@ -47,3 +47,39 @@ getval(){
         fi
     done < "$2"
 }
+
+# usage: split_str "string" "pattern"
+# description: splits string on pattern, outputs a newline separated list.
+# taken from:
+#   https://github.com/dylanaraps/pure-sh-bible#split-a-string-on-a-delimiter
+# return type: stdout string
+split_str() {
+    # Disable globbing.
+    # This ensures that the word-splitting is safe.
+    set -f
+
+    # Store the current value of 'IFS' so we
+    # can restore it later.
+    old_ifs=$IFS
+
+    # Change the field separator to what we're
+    # splitting on.
+    IFS=$2
+
+    # Create an argument list splitting at each
+    # occurrence of '$2'.
+    #
+    # This is safe to disable as it just warns against
+    # word-splitting which is the behavior we expect.
+    # shellcheck disable=2086
+    set -- $1
+
+    # Print each list value on its own line.
+    printf '%s\n' "$@"
+
+    # Restore the value of 'IFS'.
+    IFS=$old_ifs
+
+    # Re-enable globbing.
+    set +f
+}
