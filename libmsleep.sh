bbb1da067982156f7edec079b341336d11b30c98
eylles <ed.ylles1997@gmail.com>
2026-07-07 17:29:16 -0600

    Correct formatting

--- libmsleep.sh
+++ libmsleep.sh
@@ -13,12 +13,12 @@ has_usleep=$(command -v usleep)
 # description: path if available to sleep that supports floats
 has_fsleep=""
 if [ -z "$has_usleep" ] && is_program "python"; then
-  has_usleep=""
-  has_fsleep=$(command -v python)
+    has_usleep=""
+    has_fsleep=$(command -v python)
 fi
 if [ -z "$has_usleep" ] && is_program "perl"; then
-  has_usleep=$(command -v perl)
-  has_fsleep=""
+    has_usleep=$(command -v perl)
+    has_fsleep=""
 fi
 if sleep 0.001 2>/dev/null; then
     has_usleep=""
@@ -30,36 +30,36 @@ fi
 # return type: void
 msleep () {
     milisecs="$1"
-      if [ -n "$has_usleep" ]; then
-    microsecs="${milisecs}000"
-    case "$has_usleep" in
-      *usleep)
-        $has_usleep "$microsecs" >/dev/null 2>&1
-        ;;
-      *busybox)
-        $has_usleep usleep "$microsecs" >/dev/null 2>&1
-        ;;
-      *perl)
-        $has_usleep -MTime::HiRes=usleep -e 'usleep('"$microsecs"')' \
-          >/dev/null 2>&1
-        ;;
-    esac
-  else
-    sec_whole=$(( milisecs / 1000 ))
-    sec_decim=$(( milisecs % 1000 ))
-    if [ "$sec_decim" -lt 10 ]; then
-      sec_decim="00${sec_decim}"
-    elif [ "$sec_decim" -lt 100 ]; then
-      sec_decim="0${sec_decim}"
+    if [ -n "$has_usleep" ]; then
+        microsecs="${milisecs}000"
+        case "$has_usleep" in
+            *usleep)
+                $has_usleep "$microsecs" >/dev/null 2>&1
+                ;;
+            *busybox)
+                $has_usleep usleep "$microsecs" >/dev/null 2>&1
+                ;;
+            *perl)
+                $has_usleep -MTime::HiRes=usleep -e 'usleep('"$microsecs"')' \
+                    >/dev/null 2>&1
+                ;;
+        esac
+    else
+        sec_whole=$(( milisecs / 1000 ))
+        sec_decim=$(( milisecs % 1000 ))
+        if [ "$sec_decim" -lt 10 ]; then
+            sec_decim="00${sec_decim}"
+        elif [ "$sec_decim" -lt 100 ]; then
+            sec_decim="0${sec_decim}"
+        fi
+        secs="${sec_whole}.${sec_decim}"
+        case "$has_fsleep" in
+            *sleep)
+                $has_fsleep "$secs" >/dev/null 2>&1
+                ;;
+            *python)
+                $has_fsleep -c 'import time; time.sleep('"$secs"')' >/dev/null 2>&1
+                ;;
+        esac
     fi
-    secs="${sec_whole}.${sec_decim}"
-    case "$has_fsleep" in
-      *sleep)
-        $has_fsleep "$secs" >/dev/null 2>&1
-        ;;
-      *python)
-        $has_fsleep -c 'import time; time.sleep('"$secs"')' >/dev/null 2>&1
-        ;;
-    esac
-  fi
 }
